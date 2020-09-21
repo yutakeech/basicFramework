790ba2deec01c46e4b1c55c458defae78d9cc3c7
Anton <shkarin39@gmail.com>
2020-09-21 11:35:05 +0200

    2

--- app/MVC/Controllers/HelloController.php
+++ app/MVC/Controllers/HelloController.php
@@ -1,15 +1,15 @@
 <?php
 
-namespace App\MVC\Controllers;
+// namespace App\MVC\Controllers;
 
-use Framework\Controller;
+// use Framework\Controller;
 
-class HelloController extends Controller
-{
-    public function index(){
-        return $this->view('index.php',['name'=>'Иванов']);
-    }
-    public function data($user_name, $group_name){
-        return $this->view('data.php',['name'=>$user_name,'group'=>$group_name]);
-    }
-}
+// class HelloController extends Controller
+// {
+//     public function index(){
+//         return $this->view('index.php',['name'=>'Иванов']);
+//     }
+//     public function data($user_name, $group_name){
+//         return $this->view('data.php',['name'=>$user_name,'group'=>$group_name]);
+//     }
+// }
